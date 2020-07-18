552217301c702a0c46ded52a78837e1b958405cf
t4canty <t4canty@gmail.com>
2020-07-18 01:59:43 -0700

    added button actions - mostly.

--- Noppes-Json-Parser/src/t4canty/Gui.java
+++ Noppes-Json-Parser/src/t4canty/Gui.java
@@ -2,6 +2,7 @@ package t4canty;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,7 +10,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -25,6 +25,15 @@ import Noppes.Json.JsonException;
 public class Gui extends JPanel implements ActionListener{
 	private boolean debug = true;
 	private JFrame f;
+	private ButtonGroup autocorrectOptions;
+	private JButton autocorrectButton;
+	private JButton nextButton;
+	private JTextField currentMisspelledWord;
+	private int currentIndexOfWord = 0 ;
+	private dataExtractor dataE;
+	private JPanel autoCorrectButtonPanel;
+	private JTextArea jText;
+	
 	public Gui(File file) throws IOException, JsonException, InterruptedException {
 		f = new JFrame();
 		f.setTitle("CustomNPCs spellchecker");
@@ -32,38 +41,40 @@ public class Gui extends JPanel implements ActionListener{
 		
 		this.setLayout(new BorderLayout());
 		
-		dataExtractor dataE = new dataExtractor(file, false);
-		//setDoubleBuffered(false);
+		dataE = new dataExtractor(file, false);
+		setDoubleBuffered(false);
 		
-		JTextField currentMisspelledWord = new JTextField( dataE.getWords().get(0));
+		currentMisspelledWord = new JTextField( dataE.getWords().get(currentIndexOfWord));
 		JPanel buttonsAndStuffPanel = new JPanel();
-		JButton nextButton = new JButton("Next");
-		JButton autocorrectButton = new JButton("Edit");
+		nextButton = new JButton("next");
+		autocorrectButton = new JButton("edit");
 		JTextArea numFilesLeft = new JTextArea("Null");
-		ButtonGroup autocorrectOptions = new ButtonGroup();
-		JPanel autoCorrectButtonPanel = new JPanel();
 		
-		StringTokenizer st = new StringTokenizer(dataE.getCorrections().get(0), " ");
-		autoCorrectButtonPanel.setLayout(new GridLayout(st.countTokens(), 1));
-		while(st.hasMoreTokens()) {
-			JRadioButton jb = new JRadioButton(st.nextToken());
-			jb.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					System.out.println("you pressed da button - do stuff later");
-					
-				}
-			});
-			autocorrectOptions.add(jb);
-			autoCorrectButtonPanel.add(jb);
-		}
+		
+		addButtons(currentIndexOfWord);
+		
+		autocorrectButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {jText.setEditable(!jText.isEditable());}
+		});
+		
+		nextButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(currentIndexOfWord < dataE.getWords().size()) currentIndexOfWord++;
+				else currentIndexOfWord = 0;
+				autoCorrectButtonPanel = null;
+//				currentMisspelledWord.setText(dataE.getWords().get(currentIndexOfWord));
+//				addButtons(currentIndexOfWord);
+//				buttonsAndStuffPanel.revalidate();
+			}
+		});
 		
 		JScrollPane buttonScroller = new JScrollPane(autocorrectButton);
 		buttonScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
 		
-		JTextArea jText = new JTextArea(10, 10);
+		jText = new JTextArea(10, 10);
 		jText.setText("Dialouge Text: \n" + dataE.dailougeText + "\n Dialouge Options: \n");
 		jText.setEditable(false);
 		jText.setLineWrap(true);
@@ -96,7 +107,7 @@ public class Gui extends JPanel implements ActionListener{
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
 
-		Timer t = new Timer(13, (ActionListener) this);
+		Timer t = new Timer(34, (ActionListener) this);
 		t.start();
 	}
 	public static void main(String args[]) {
@@ -108,5 +119,34 @@ public class Gui extends JPanel implements ActionListener{
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		if(currentIndexOfWord >= dataE.getWords().size()) currentIndexOfWord = 0;
+	}
+	
+	@Override
+	public void paint(Graphics arg0) {
+		autoCorrectButtonPanel.revalidate();
+		System.out.println("motherfucker");
+		super.paint(arg0);
+	}
+	
+	private void addButtons(int index) {
+		System.out.println("Gui.addButtons()");
+		autoCorrectButtonPanel = new JPanel();
+		autocorrectOptions = new ButtonGroup();
+		StringTokenizer st = new StringTokenizer(dataE.getCorrections().get(index), " ");
+		autoCorrectButtonPanel.setLayout(new GridLayout(st.countTokens(), 1));
+		while(st.hasMoreTokens()) {
+			System.out.println("adding buttons");
+			JRadioButton jb = new JRadioButton(st.nextToken());
+			jb.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					currentMisspelledWord.setText(jb.getText());
+				}
+			});
+			autocorrectOptions.add(jb);
+			autoCorrectButtonPanel.add(jb);
+		}
 	}
 }
